5acc911fcea756dff91ce5b4305163496b721e95
Savant Krishna <savant.2020@gmail.com>
2014-01-22 18:19:38 +0530

    some changes

--- .bin/commit_folders.sh
+++ .bin/commit_folders.sh
@@ -3,7 +3,7 @@ branch=$1
 shift
 folders=$(git status | grep \# | tail -n +5 | cut -b3-)
 for f in $folders; do
-    git add f;
+    git add $f;
     git commit -m "added $f";
     for remote in "$@"; do
         git push $remote $branch;
--- .bin/setup.sh
+++ .bin/setup.sh
@@ -1,7 +1,8 @@
 wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add - 
 sudo sh -c 'echo "deb http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
 sudo apt-get update
-sudo apt-get install curl vim gconf-editor python-pip git gitk ruby ntfs-config google-chrome-beta eiskaltdcpp terminator guake indicator-cpufreq acpi vlc openssh-server axel tree texlive-full pandoc socat powertop gparted
+sudo apt-get install curl vim gconf-editor python-pip git gitk ruby ntfs-config google-chrome-beta eiskaltdcpp terminator guake indicator-cpufreq acpi vlc openssh-server axel tree texlive-full pandoc socat powertop gparted markdown dconf-tools ia32-libs
 sudo add-apt-repository ppa:myunity/ppa
 sudo apt-get update
 sudo apt-get install myunity
+sudo pip install markdown2
