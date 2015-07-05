9254c249d2f92e05a7e949bfcb20239d292f0fe0
Savant Krishna <savant.2020@gmail.com>
2015-07-05 13:43:20 +1000

    add more packages for setup

--- .bin/setup.sh
+++ .bin/setup.sh
@@ -1,8 +1,8 @@
 #wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add - 
 #sudo sh -c 'echo "deb http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
-#sudo apt-get update
-sudo apt-get install curl vim gconf-editor python-pip git gitk ruby ntfs-config google-chrome-beta eiskaltdcpp terminator guake indicator-cpufreq acpi vlc openssh-server axel tree pandoc socat powertop gparted markdown dconf-tools ia32-libs python3-dev w3m mercurial git-svn mercurial-git libxvidcore-dev libfaad-dev libfaac-dev libmp3lame-dev mplayer mencoder intltool
-sudo add-apt-repository ppa:myunity/ppa
 sudo apt-get update
-sudo apt-get install myunity
+sudo apt-get install curl vim gconf-editor python-pip git gitk ruby ntfs-config google-chrome-beta eiskaltdcpp terminator guake indicator-cpufreq acpi vlc openssh-server axel tree pandoc socat powertop gparted markdown dconf-tools  python3-dev w3m mercurial git-svn mercurial-git libxvidcore-dev libfaad-dev libfaac-dev libmp3lame-dev mplayer mencoder intltool python-dev zsh cmake ninja-build libboost-all-dev libcurl4-openssl-dev unity-tweak-tool
+#sudo add-apt-repository ppa:myunity/ppa 
+#sudo apt-get update
+#sudo apt-get install myunity
 sudo pip install markdown2
